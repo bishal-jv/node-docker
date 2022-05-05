f3d23e3f227d7f5725b997466e5cf58d570a67d3
Bishal <bishalnath.javaventures@gmail.com>
2022-05-05 09:38:35 -0400

    updated node api's

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000;
 
 app.get('/', (req,res) => res.send('hello world'));
+app.get('/next', (req,res) => res.send('This is the next page'));
 
 app.listen(port, () => {
     console.log('API running on:', port)
